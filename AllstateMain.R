663f01e301a3927661af9cb37a66f078cf286f45
wacax <wacax@hotmail.com>
2014-05-05 15:17:32 -0500

    first commit

---
+++ AllstateMain.R
@@ -0,0 +1,27 @@
+#Allstate Competition
+#ver 0.1
+
+#########################
+#Init
+rm(list=ls(all=TRUE))
+
+#Load/install libraries
+require("ggplot2")
+require("gbm")
+require("Metrics")
+
+#Set Working Directory
+workingDirectory <- '/home/wacax/Documents/Wacax/Kaggle Data Analysis/Allstate/Allstate'
+setwd(workingDirectory)
+
+dataDirectory <- '/home/wacax/Documents/Wacax/Kaggle Data Analysis/Allstate/Data/'
+
+#Load functions
+
+###########################
+#Load Data
+#Input Data
+train <- read.csv(paste0(dataDirectory, 'train.csv'), header = TRUE, stringsAsFactors = FALSE, na.strings=c("", "NA", "NULL"))
+test <- read.csv(paste0(dataDirectory, 'test_v2.csv'), header = TRUE, stringsAsFactors = FALSE, na.strings=c("", "NA", "NULL"))
+
+submissionTemplate <- read.csv(paste0(dataDirectory, 'sampleSubmission.csv'), header = TRUE, stringsAsFactors = FALSE, na.strings=c("", "NA", "NULL"))
